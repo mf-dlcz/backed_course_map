1907fc871bb42a2346fc654aa554d4acc592483b
Maria Delacruz <mar23iaa@gmail.com>
2026-07-14 00:04:06 -0700

    Added a new file

---
+++ exercises/chap_1/counting_items_list.py
@@ -0,0 +1,12 @@
+#Counting the Items in a List
+
+'''
+Assignment:
+Our players need a way to see how many copies of a specific item they have within their inventory!
+
+Let's finish the get_item_counts function.
+
+Within the loop, check if the items are the strings "Potion", "Bread", or "Shortsword".
+If you find a match, increment the corresponding counter, either potion_count, bread_count or shortsword_count.
+'''
+
