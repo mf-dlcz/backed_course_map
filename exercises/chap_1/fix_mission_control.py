ff56c25d1d02e03a851cf9bf68818b7da4eb6cd7
Maria Delacruz <mar23iaa@gmail.com>
2026-07-14 23:58:11 -0700

    New problem

---
+++ exercises/chap_1/fix_mission_control.py
@@ -0,0 +1,19 @@
+'''
+The functions should:
+
+return the correct value instead of the wrong one
+use the correct parameter in the calculation
+build the exact expected string
+fix one scope bug where a function reads the wrong variable name
+Do not use:
+
+- if statements
+- loops
+lists, dictionaries, or sets
+
+Expected behavior
+make_badge(name, title)
+Return a string in this format:
+
+"{title}: {name}"
+'''
