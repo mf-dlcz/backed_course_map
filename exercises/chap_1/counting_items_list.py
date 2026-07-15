d6182c892ec97d58613caee6d739b158ae6c51a4
Maria Delacruz <mar23iaa@gmail.com>
2026-07-14 23:54:30 -0700

    Completed problem

--- exercises/chap_1/counting_items_list.py
+++ exercises/chap_1/counting_items_list.py
@@ -10,3 +10,21 @@ Within the loop, check if the items are the strings "Potion", "Bread", or "Short
 If you find a match, increment the corresponding counter, either potion_count, bread_count or shortsword_count.
 '''
 
+def get_item_counts(items):
+    potion_count = 0
+    bread_count = 0
+    shortsword_count = 0
+
+    # don't touch above this line
+
+    for i in range(0, len(items)):
+        if items[i] == "Potion":
+            potion_count += 1
+        elif items[i] == "Bread":
+            bread_count += 1
+        elif items[i] == "Shortsword":
+            shortsword_count += 1
+
+    # don't touch below this line
+
+    return potion_count, bread_count, shortsword_count
