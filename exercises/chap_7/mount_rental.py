858b517c0194f34910466b3194622e733e187e2c
Maria Delacruz <mar23iaa@gmail.com>
2026-08-13 00:48:11 -0700

    Completed another exercise

---
+++ exercises/chap_7/mount_rental.py
@@ -0,0 +1,24 @@
+"""
+Mount Rental
+
+Players can rent other players' flying mounts from the "Uper" ride sharing guild, but they must return 
+the mount before they have used up all their time, or they will be charged a fee.
+
+-> Assignment
+Complete the check_mount_rental function. It takes two inputs:
+
+time_used – the amount of time the mount has been used in minutes
+
+time_purchased – the amount of time the character rented the mount for
+
+If time_used meets or exceeds time_purchased, then the rental is expired and greedy Uper will 
+
+charge a fee, so the function should return the string "overtime charged"
+
+Otherwise, return the string "no charges yet"
+"""
+
+def check_mount_rental(time_used, time_purchased):
+    if time_used >= time_purchased:
+        return "overtime charged"
+    return "no charges yet"
