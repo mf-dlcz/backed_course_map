44c2905cbb569472eff81fe15e1a83a610e34f96
Maria Delacruz <mar23iaa@gmail.com>
2026-08-20 23:39:19 -0700

    Completed lists

---
+++ exercises/chap_9/lists.py
@@ -0,0 +1,11 @@
+"""
+LISTS
+
+Let's work on Fantasy Quest's inventory! We can store items the player is carrying in a list!
+
+Fix our get_inventory function by adding Shortsword to the end of the list.
+
+"""
+
+def get_inventory():
+    return ["Healing Potion", "Leather Scraps", "Iron Helmet", "Shortsword"]
