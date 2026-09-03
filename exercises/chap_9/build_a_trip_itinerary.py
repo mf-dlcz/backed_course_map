8999456b659377fe98d94a94c904fc8ec183085f
Maria Delacruz <mar23iaa@gmail.com>
2026-09-03 09:20:52 -0700

    Began a new exercise

---
+++ exercises/chap_9/build_a_trip_itinerary.py
@@ -0,0 +1,24 @@
+"""
+
+Builds a trip itinerary.
+
+Complete the build_itinerary function.
+
+It should build and return a new list from:
+
+A list of destination names
+The number of times each destination should appear
+Loop through the destinations in order. Use .append() to add each destination to the itinerary the requested number of times.
+
+Return an empty list if there are no destinations or if the repeat count is zero or negative.
+
+For example:
+
+build_itinerary(["Paris", "Rome"], 2)
+# ["Paris", "Paris", "Rome", "Rome"]
+
+build_itinerary(["Tokyo"], 3)
+# ["Tokyo", "Tokyo", "Tokyo"]
+
+"""
+
