32b419f50d9be2fe0fb73da2f89e86fcda211260
Maria Delacruz <mar23iaa@gmail.com>
2026-09-09 23:43:48 -0700

    Began find the coolest temp

---
+++ exercises/chap_9/find_coolest_temp.py
@@ -0,0 +1,21 @@
+"""
+Find the Coolest Temperature
+Complete the find_coolest_temperature function.
+
+It takes a non-empty list of temperatures and returns the lowest temperature.
+
+Use a loop and comparison operators to find the answer. Do not use Python's built-in min function.
+
+For example:
+
+temperatures = [18, 12, 20, 7, 15]
+print(find_coolest_temperature(temperatures))
+# 7
+
+Temperatures may be negative:
+
+print(find_coolest_temperature([-2, -9, 3, 0]))
+# -9
+
+
+"""
