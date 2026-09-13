ec2d28916c40a68fd9fe6c4e52ae110defa1f7fb
Maria Delacruz <mar23iaa@gmail.com>
2026-09-12 23:32:41 -0700

    Completed List Concatination

---
+++ exercises/chap_9/list_contatination.py
@@ -0,0 +1,16 @@
+"""
+
+List Contatination:
+
+Finish the concatenate_favorites function. 
+
+It takes three different lists – the player's favorite_weapons, favorite_armor and favorite_items.
+
+Create a new list that combines the lists favorite_weapons, favorite_armor, and favorite_items in this order.
+
+Return the list containing the combined favorites.
+
+"""
+
+def concatenate_favorites(favorite_weapons, favorite_armor, favorite_items):
+    return favorite_weapons + favorite_armor + favorite_items
