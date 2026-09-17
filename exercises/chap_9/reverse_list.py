a9522a5de59b15f7b5e708227ce8c72a51318790
Maria Delacruz <mar23iaa@gmail.com>
2026-09-16 23:57:59 -0700

    Began Reverse List

---
+++ exercises/chap_9/reverse_list.py
@@ -0,0 +1,15 @@
+"""
+
+Reverse List:
+
+Some of our players would like to view their inventories in reverse order.
+
+Using a loop, let's write a function that takes a list as an input and returns a new list except all the items are in reverse order.
+
+For example:
+
+[1, 2, 3] -> [3, 2, 1]
+['a', 'b', 'c', 'd'] -> ['d', 'c', 'b', 'a']
+
+"""
+
